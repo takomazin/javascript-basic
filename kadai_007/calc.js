68ffe7329e534aa99ff54fead48363e22b7f8ec2
takomazin <uta_manato@koushi-intec.co.jp>
2025-05-29 13:22:48 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,24 @@
+// 変数numの宣言
+let num = 15;
+
+// 変数numが3と5の倍数の場合
+if (num % 3 == 0 && num % 5 == 0){
+  console.log('3と5の倍数です');
+}
+
+// 変数numが3の倍数の場合
+else if (num % 3 == 0){
+  console.log('3の倍数です');
+}
+
+// 変数numが5の倍数の場合
+else if(num % 5 == 0){
+  console.log('5の倍数です');
+}
+
+//それ以外の場合
+else {
+  console.log(num);
+}
+
+
